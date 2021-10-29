5ab6501319ff8dbf0eb1977798ffced105fc962a
kosmos1308 <pasha13kosmos@gmail.com>
2021-10-29 15:11:00 +0300

    Добавил проект

---
+++ homework-1/homework-1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  homework-1
+//
+//  Created by pavel on 29.10.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
