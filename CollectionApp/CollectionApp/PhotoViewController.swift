9d409e670aca51327d73ff5f0139ebbd90bb9dc9
kosmos1308 <pasha13kosmos@gmail.com>
2021-11-19 16:56:57 +0300

    Создал collectionView

--- CollectionApp/CollectionApp/PhotoViewController.swift
+++ CollectionApp/CollectionApp/PhotoViewController.swift
@@ -8,18 +8,72 @@
 import UIKit
 
 class PhotoViewController: UIViewController {
+    
+    private var collectionView: UICollectionView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.backgroundColor = .white
+        updateNavController()
+        setupCollectionView()
+        
+    }
+    
+    //MARK: - update navigationController
+    private func updateNavController() {
         navigationController?.navigationBar.backgroundColor = .systemGray5
         navigationController?.navigationBar.prefersLargeTitles = true
         title = "Photo"
-        view.backgroundColor = .white
-        
     }
-
     
+    //MARK: - setup collectionView
+    private func setupCollectionView() {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        guard let collectionView = collectionView else {return}
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(collectionView)
+        setupCollectionViewAutoLayout()
+    }
+    
+    
+    //MARK: - setup autoLayout collectionView
+    private func setupCollectionViewAutoLayout() {
+        collectionView?.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        collectionView?.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        collectionView?.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        collectionView?.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
+    
+    
+    //MARK: -
+    //MARK: -
+    //MARK: -
+}
+
+extension PhotoViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .systemPink
+        return cell
+    }
+}
+
+extension PhotoViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(indexPath.item)
+    }
     
 }
 
