49ed7cd9bb42131b7d23f24c23a176370f930735
kosmos1308 <pasha13kosmos@gmail.com>
2021-11-27 16:28:08 +0300

    Создал проект

---
+++ CollectionAppWithArchitectures/CollectionAppWithArchitectures/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionAppWithArchitectures
+//
+//  Created by pavel on 27.11.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+    }
+
+
+}
+
