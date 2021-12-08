e74fc6a09733332137d94dfe311ddaa00546fa67
kosmos1308 <pasha13kosmos@gmail.com>
2021-12-08 13:47:11 +0300

    Создал проект

---
+++ chooseCar/chooseCar/ChooseViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ChooseViewController.swift
+//  chooseCar
+//
+//  Created by pavel on 8.12.21.
+//
+
+import UIKit
+
+final class ChooseViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+    }
+
+}
+
