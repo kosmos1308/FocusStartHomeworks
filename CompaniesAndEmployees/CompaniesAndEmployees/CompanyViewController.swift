d3049a19bf07a3d3825d568c90234e80c619f704
kosmos1308 <pasha13kosmos@gmail.com>
2021-12-22 17:38:58 +0300

    Создал проект

---
+++ CompaniesAndEmployees/CompaniesAndEmployees/CompanyViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CompanyViewController.swift
+//  CompaniesAndEmployees
+//
+//  Created by pavel on 22.12.21.
+//
+
+import UIKit
+
+class CompanyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .systemBackground
+        navigationItem.title = "Companies"
+    }
+
+}
---
+++ CompaniesAndEmployees/CompaniesAndEmployees/EmployeeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  EmployeeViewController.swift
+//  CompaniesAndEmployees
+//
+//  Created by pavel on 22.12.21.
+//
+
+import UIKit
+
+class EmployeeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+}
